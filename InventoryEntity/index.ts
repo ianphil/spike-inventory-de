900ce7caa4cd166a26e5fdc00d2beef1b3cfdc37
David Khourshid <davidkpiano@gmail.com>
2020-04-15 10:25:42 -0400

    Add durable entity scaffolding

---
+++ InventoryEntity/index.ts
@@ -0,0 +1,20 @@
+﻿import * as df from 'durable-functions';
+
+const entityFunction = df.entity((context) => {
+  const currentValue = context.df.getState(() => 0);
+  switch (context.df.operationName) {
+    case 'add':
+      const amount = context.df.getInput();
+      // @ts-ignore
+      context.df.setState(currentValue + amount);
+      break;
+    case 'reset':
+      context.df.setState(0);
+      break;
+    case 'get':
+      context.df.return(currentValue);
+      break;
+  }
+});
+
+export default entityFunction;

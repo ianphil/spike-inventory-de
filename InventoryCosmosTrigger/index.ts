8abbc5abf53dd8eacbe667731d929a173e2d8dfd
David Khourshid <davidkpiano@gmail.com>
2020-04-20 14:53:30 -0400

    Add store-specific counting POC

--- InventoryCosmosTrigger/index.ts
+++ InventoryCosmosTrigger/index.ts
@@ -1,11 +1,20 @@
 import { AzureFunction, Context } from '@azure/functions';
+import * as df from 'durable-functions';
 
 const cosmosDBTrigger: AzureFunction = async function(
   context: Context,
   documents: any[]
 ): Promise<void> {
+  const client = df.getClient(context);
+
   if (!!documents && documents.length > 0) {
+    const { storeId, count } = documents[0];
+    context.log('Store id: ', storeId);
+    context.log('Store count: ', count);
     context.log('Document Id: ', documents[0].id);
+
+    const entityId = new df.EntityId('InventoryEntity', `${storeId}`);
+    client.signalEntity(entityId, 'add', { amount: count });
   }
 };
 
